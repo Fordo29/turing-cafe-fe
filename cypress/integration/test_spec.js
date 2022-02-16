70074714dd8833a9b4935c97c0a8a56361ee007d
Christine Rowland <fordo29@gmail.com>
2022-02-16 11:56:03 -0700

    Add tests for display and making a reservation

---
+++ cypress/integration/test_spec.js
@@ -0,0 +1,55 @@
+describe('Turing Cafe Reservation Displays', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+  })
+
+  it('Should be able to visit the main page and the user will see the application name', () => {
+    cy.contains('Turing Cafe Reservations')
+  });
+
+   it('Should display a reservation card with users name', () => {
+    cy.get('.card')
+      .contains('Christie')
+  });
+
+  it('Should have a date for reservation', () => {
+      cy.get('.card')
+      .contains('12/29')
+  });
+
+  it('Should have a time for the reservation', () => {
+    cy.get('.card')
+      .contains('7:00 pm')
+  });
+
+  
+  it('Should display number of guests', () => {
+    cy.get('.card')
+      .contains('Number of Guests: 12')
+  });
+
+    it('Should display a cancel button', () => {
+    cy.get('.card')
+      .find('button').contains('Cancel')
+  });
+  
+  it('Should display 9 reservation cards', () => {
+    cy.get('.card')
+      .should('have.length', 9)
+  }); 
+
+    it('Should have input fields to enter a new reservation and a new reservation is created', () => {
+        cy.get('input').get('.name-input').type('Christine')
+        cy.get('input').get('.date-input').type('12/26')
+        cy.get('input').get('.time-input').type('8')
+        cy.get('input').get('.number-input').type('7')
+        cy.get('button').contains("Make a reservation").click()
+         cy.get('.card').contains('Christine')
+    });
+  
+
+});
+
+
+// Write a test that checks that when data is put into the form, the value is reflected in that form input.
+// Write a test to check the user flow of adding a new reservation to the page.
